09ed0a0fe6d6d5593cdb645405232aba0d8628fc
David Eason <david.eason@mulesoft.com>
2013-03-26 12:56:52 -0700

    Updated ShippingOrderConfirmation.groovy for DM preview capability

--- src/test/resources/ShippingOrderConfirmation.groovy
+++ src/test/resources/ShippingOrderConfirmation.groovy
@@ -3,9 +3,10 @@ def shippingOrder = new org.ordermgmt.ShippingOrder(
 	shippingId = '2324234',
     billingAddress = new org.ordermgmt.Address(name='Company1_Name', line1 ='34 Smith St', line2 ='Apt.2', city='Fresno', stateOrProvinceCode='CA', countryCode='USA', postalCode='94537'),
     shippingAddress = new org.ordermgmt.Address(name='Company2_Name', line1 ='88 Smith St', line2 ='Apt.3', city='Chicago', stateOrProvinceCode='CA', countryCode='USA', postalCode='94537'),
-    order = new org.ordermgmt.Order(orderItemList=[new org.ordermgmt.OrderItem(merchantSKU:'1234', quantity:200),new org.ordermgmt.OrderItem(merchantSKU:'3334', quantity:200)]
+    order = new org.ordermgmt.Order(
+    	orderItemList=[new org.ordermgmt.OrderItem(merchantSKU:'1234', quantity:200),new org.ordermgmt.OrderItem(merchantSKU:'3334', quantity:200)]
     )
    )
 
-return shippingOrder
+return new org.ordermgmt.ShippingOrderConfirmation(shippingOrder,true);
    
